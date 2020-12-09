e00ae85e3d870ddb0b107d532680123c8f3047fa
ugurkarakurt <ugurkarakurt57@gmail.com>
2020-12-09 17:17:06 +0300

    - Feat: Animate css added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM, { unstable_batchedUpdates } from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'alertifyjs/build/css/alertify.min.css'
+import "animate.css/animate.min.css";
 import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
